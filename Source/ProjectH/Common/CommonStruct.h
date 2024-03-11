f3da73124633b930b3430b4f55463fd95d3f982b
biomaticals <biomaticals@naver.com>
2024-03-12 01:11:57 +0900

    Anatomy 관련 구조체, 열거형 분석

---
+++ Source/ProjectH/Common/CommonStruct.h
@@ -0,0 +1,12 @@
+// Copy Rigts are in Team UniqueTurtle. 
+
+#include "CoreMinimal.h"
+#include "CommonStruct.generated.h"
+
+#pragma region Character Customizing
+//USTRUCT(BlueprintType)
+//struct FAnatomy
+//{
+//	GENERATED_BODY()
+//};
+#pragma endregion
