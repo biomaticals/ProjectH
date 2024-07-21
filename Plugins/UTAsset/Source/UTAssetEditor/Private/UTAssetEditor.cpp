9c61bcf4260565aa31e81bb771ef21facf153cc5
biomaticals <biomaticals@naver.com>
2024-07-21 21:10:39 +0900

    UTAsset 플러그인, UTAsset, UTAssetEditor 모듈 추가

---
+++ Plugins/UTAsset/Source/UTAssetEditor/Private/UTAssetEditor.cpp
@@ -0,0 +1,20 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UTAssetEditor.h"
+
+#define LOCTEXT_NAMESPACE "FUTAssetEditorModule"
+
+void FUTAssetEditorModule::StartupModule()
+{
+	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
+}
+
+void FUTAssetEditorModule::ShutdownModule()
+{
+	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
+	// we call this function before unloading the module.
+}
+
+#undef LOCTEXT_NAMESPACE
+	
+IMPLEMENT_MODULE(FUTAssetEditorModule, UTAssetEditor)
