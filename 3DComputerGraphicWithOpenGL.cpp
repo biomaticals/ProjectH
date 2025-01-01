aaeba892472a34df78df55a030113de93d6c484c
biomaticals <biomaticals@naver.com>
2025-01-02 02:20:58 +0900

    GLUT, GLEW 설치 및 샘플 코드 작성

---
+++ 3DComputerGraphicWithOpenGL.cpp
@@ -0,0 +1,26 @@
+﻿// Copyright 2025. Team Unique Turtle ; https://github.com/biomaticals. All rights reserved.
+// All contents cannot be copied, distributed, revised.
+
+#include "iostream"
+#include "GL/glut.h"
+
+void MyDisplay()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    glBegin(GL_POLYGON);
+    glVertex3f(-0.5, -0.5, 0.0);
+    glVertex3f(0.5, -0.5, 0.0);
+    glVertex3f(0.5, 0.5, 0.0);
+    glVertex3f(-0.5, 0.5, 0.0);
+    glEnd();
+    glFlush();
+}
+
+int main(int argc, char** argv)
+{
+    glutInit(&argc, argv);
+    glutCreateWindow("OpenGL Drawing Example");
+    glutDisplayFunc(MyDisplay);
+    glutMainLoop();
+    return 0;
+}
