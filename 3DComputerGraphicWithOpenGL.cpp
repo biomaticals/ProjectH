b8b0ce9f0c743649fbcf73c33c7bbe1e51b99697
biomaticals <biomaticals@naver.com>
2025-01-02 23:48:41 +0900

    상태 변수와 관련된 숨겨진 명령을 추가하여 기능 확인, glViewport를 사용하여 윈도우 변환시 왜곡 현상 확인

--- 3DComputerGraphicWithOpenGL.cpp
+++ 3DComputerGraphicWithOpenGL.cpp
@@ -7,6 +7,21 @@
 void MyDisplay()
 {
     glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(1.0, 1.0, 1.0);
+    glBegin(GL_POLYGON);
+    glVertex3f(-0.5, -0.5, 0.0);
+    glVertex3f(0.5, -0.5, 0.0);
+    glVertex3f(0.5, 0.5, 0.0);
+    glVertex3f(-0.5, 0.5, 0.0);
+    glEnd();
+    glFlush();
+}
+
+void MyDisplay2()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    glViewport(0, 0, 150, 150);
+    glColor3f(1.0, 1.0, 1.0);
     glBegin(GL_POLYGON);
     glVertex3f(-0.5, -0.5, 0.0);
     glVertex3f(0.5, -0.5, 0.0);
@@ -18,9 +33,29 @@ void MyDisplay()
 
 int main(int argc, char** argv)
 {
+    // 책의 내용대로 glViewport를 명시적으로 사용하지 않은 상태로 윈도우를 조절하면 종횡비를 유지하려고 하기 때문에 왜곡이 발생하고,
+    // glViewport를 명시적으로 사용하면 물체에 왜곡이 일어나지 않고 그대로 유지됨을 확인할 수 있습니다.
+
     glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGB);
+    glutInitWindowSize(300, 300);
+    glutInitWindowPosition(0, 0);
     glutCreateWindow("OpenGL Drawing Example");
+    glClearColor(0.0, 0.0, 0.0, 1.0);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0);
     glutDisplayFunc(MyDisplay);
+
+    glutInitDisplayMode(GLUT_RGB);
+    glutInitWindowSize(600, 600);
+    glutInitWindowPosition(0, 0);
+    glutCreateWindow("OpenGL Drawing Example2");
+    glClearColor(0.0, 0.0, 0.0, 1.0);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0);
+    glutDisplayFunc(MyDisplay2);
     glutMainLoop();
     return 0;
 }
