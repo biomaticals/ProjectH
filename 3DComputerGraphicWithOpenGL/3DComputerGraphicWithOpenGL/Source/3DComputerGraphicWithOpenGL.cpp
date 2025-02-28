05ffedd43a70e2cfaecd06ff1c9068f189042e60
biomaticals <biomaticals@naver.com>
2025-03-01 03:08:49 +0900

    폴더 구조 정리

--- 3DComputerGraphicWithOpenGL/3DComputerGraphicWithOpenGL/Source/3DComputerGraphicWithOpenGL.cpp
+++ 3DComputerGraphicWithOpenGL/3DComputerGraphicWithOpenGL/Source/3DComputerGraphicWithOpenGL.cpp
@@ -1,4 +1,4 @@
-﻿// Copyright 2025. Team Unique Turtle ; https://github.com/biomaticals. All rights reserved.
+// Copyright 2025. Team Unique Turtle ; https://github.com/biomaticals. All rights reserved.
 // All contents cannot be copied, distributed, revised.
 
 #include "iostream"
