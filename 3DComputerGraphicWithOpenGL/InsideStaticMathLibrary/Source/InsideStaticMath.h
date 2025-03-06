137ea0e5c04a8046ec63f0c6ed1aa4b3be530e1a
biomaticals <biomaticals@naver.com>
2025-03-07 01:00:09 +0900

    솔루션 내 InsideStaticMath 정적 라이브러리 생성 및 컴파일 완료

---
+++ 3DComputerGraphicWithOpenGL/InsideStaticMathLibrary/Source/InsideStaticMath.h
@@ -0,0 +1,23 @@
+// Copyright 2025. Team Unique Turtle ; https://github.com/biomaticals. All rights reserved.
+// All contents cannot be copied, distributed, revised.
+
+#pragma once
+
+namespace InsideStaticMath
+{
+    class Arithmetic
+    {
+    public:
+        // Returns a + b
+        static double Add(double a, double b);
+
+        // Returns a - b
+        static double Subtract(double a, double b);
+
+        // Returns a * b
+        static double Multiply(double a, double b);
+
+        // Returns a / b
+        static double Divide(double a, double b);
+    };
+}
