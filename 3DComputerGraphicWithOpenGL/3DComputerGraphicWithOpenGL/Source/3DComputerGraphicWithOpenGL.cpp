09674c8c19aa1a2730fb5ea6d84da6a8f1cb0c4d
biomaticals <biomaticals@naver.com>
2025-04-09 23:49:32 +0900

    glfw 내부 이식 작업

--- 3DComputerGraphicWithOpenGL/3DComputerGraphicWithOpenGL/Source/3DComputerGraphicWithOpenGL.cpp
+++ 3DComputerGraphicWithOpenGL/3DComputerGraphicWithOpenGL/Source/3DComputerGraphicWithOpenGL.cpp
@@ -5,6 +5,8 @@
 #include "GL/glut.h"
 #include <stdio.h>
 
+#include "ImgUtil.h"
+
 // Static Linked Library
 #include "OutsideStaticMath.h"
 #include "InsideStaticMath.h"
@@ -13,76 +15,12 @@
 #include "InsideDynamicMath.h"
 #include "OutsideDynamicMath.h"
 
-static GLint TopLeftX, TopLeftY, BottomRightX, BottomRightY;
-
-void MyDisplay()
-{
-	glClear(GL_COLOR_BUFFER_BIT);
-	glViewport(0, 0, 300, 300);
-	glColor3f(0.5, 0.5, 0.5);
-	glBegin(GL_POLYGON);
-		glVertex3f(TopLeftX/300.0, (300.0 - TopLeftY)/300.0, 0.0);
-		glVertex3f(TopLeftX/300.0, (300.0 - BottomRightY)/300.0, 0.0);
-		glVertex3f(BottomRightX/300.0, (300.0-BottomRightY)/300.0, 0.0);
-		glVertex3f(BottomRightX/300.0, (300.0-TopLeftY)/300.0, 0.0);
-	glEnd();
-	glFlush();
-}
-
-void MyKeyboard(unsigned char KeyPressed, int X, int Y)
-{
-	switch (KeyPressed)
-	{
-		case 'Q' : exit(0); break;
-		case 'q' : exit(0); break;
-		case 27 : exit(0); break;
-	}
-}
-
-void MyMouseClick(GLint Button, GLint State, GLint X, GLint Y)
-{
-	if (Button == GLUT_LEFT_BUTTON && State == GLUT_DOWN)
-	{
-		TopLeftX = X;
-		TopLeftY = Y;
-	}
-}
-
-void MyMouseMove(GLint X, GLint Y)
-{
-	BottomRightX = X;
-	BottomRightY = Y;
-	glutPostRedisplay();
-}
-
-void MyReshape(int NewWidth, int NewHeight)
-{
-	glViewport(0, 0, NewWidth, NewHeight);
-	GLfloat WidthFactor = (GLfloat)NewWidth / (GLfloat)300;
-	GLfloat HeightFactor = (GLfloat)NewHeight / (GLfloat)300;
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	glOrtho(-1.0 * WidthFactor, 1.0 * WidthFactor, -1.0 * HeightFactor, 1.0 * HeightFactor, -1.0, 1.0);
-}
+#include "GLFW"
 
 int main(int argc, char** argv)
 {
 	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_RGB);
-	glutInitWindowSize(300, 300);
-	glutInitWindowPosition(0, 50);
-	glutCreateWindow("[코드5-7] 마우스 콜백");
-	const char* version = (const char*)glGetString(GL_VERSION);
-	std::cout << "OpenGL Version: " << version << std::endl;
-	glClearColor(1.0, 1.0, 1.0, 1.0);
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	glOrtho(0.0, 1.0, 0.0, 1.0, -1.0, 1.0);
-	glutDisplayFunc(MyDisplay);
-	glutMouseFunc(MyMouseClick);
-	glutMotionFunc(MyMouseMove);
-	//glutReshapeFunc(MyReshape);
-	glutKeyboardFunc(MyKeyboard);
+
 	glutMainLoop();
 	return 0;
 }
