fea6bcb9ecbcc4be5476ef3da7c37f3797bfdba6
biomaticals <biomaticals@naver.com>
2025-04-10 22:59:54 +0900

    glfw로 전환 성공

--- 3DComputerGraphicWithOpenGL/3DComputerGraphicWithOpenGL/Source/3DComputerGraphicWithOpenGL.cpp
+++ 3DComputerGraphicWithOpenGL/3DComputerGraphicWithOpenGL/Source/3DComputerGraphicWithOpenGL.cpp
@@ -1,11 +1,10 @@
 // Copyright 2025. Team Unique Turtle ; https://github.com/biomaticals. All rights reserved.
 // All contents cannot be copied, distributed, revised.
-
+#include <stdio.h>
 #include "iostream"
 #include "GL/glut.h"
-#include <stdio.h>
 
-#include "ImgUtil.h"
+
 
 // Static Linked Library
 #include "OutsideStaticMath.h"
@@ -15,12 +14,35 @@
 #include "InsideDynamicMath.h"
 #include "OutsideDynamicMath.h"
 
-#include "GLFW"
-
-int main(int argc, char** argv)
+#include <GLFW/glfw3.h>
+ 
+void render(GLFWwindow* window);
+ 
+int main()
 {
-	glutInit(&argc, argv);
-
-	glutMainLoop();
-	return 0;
+    // glfw 초기화
+    glfwInit();
+ 
+    // window 만들기 (width, height, title, monitor, share)
+    GLFWwindow* window = glfwCreateWindow(640, 480, "Inyong", 0, 0);
+    
+    // context는 그리기 위한 내용을 담고 있음
+    // window가 두개이면 context가 두개일수도 있음
+    // 그러면 어떤 context로 그릴지 정해줘야 함
+    glfwMakeContextCurrent(window);
+ 
+    // 사용자가 window 창을 닫을 때까지
+    while (!glfwWindowShouldClose(window)) {
+        // window 그려주기
+        render(window);
+ 
+        // 마우스 움직이는 것. 이런것들 들고와서 필요한 window한테 보내주기
+        glfwPollEvents();
+    }
+ 
+    // window가 닫혔을 경우 끝내자
+    glfwDestroyWindow(window);
+    glfwTerminate();
 }
+ 
+void render(GLFWwindow* window) { }
